1881280f53ee7dcdc0b7ad8f8ebfed4a59a227df
Evan <evanjensen95@gmail.com>
2021-06-29 11:31:00 -0700

    second

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const Rollbar = require('rollbar')
+const rollbar = new Rollbar({
+  accessToken: '05130feae549441590812dfbd2b72b43',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
 
 app.use(express.json())
 
+
+
 app.get('/', function(req, res){
+    rollbar.log('hello world')
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
